c9f860342fcea58144ecdcec59c80204e5203b60
mvmmtbhau <nguyen1hau2211@gmail.com>
2023-03-20 13:08:26 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>Hello, Vuejs</h1>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
